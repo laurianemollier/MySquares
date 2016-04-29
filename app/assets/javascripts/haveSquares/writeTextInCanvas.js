5c95c6e4512216f2247d1ec77e161396697ea24f
Lauriane Mollier <mollierlauriane@gmail.com>
2016-04-29 21:55:05 +0300

    begin the possibly of moving the text

--- app/assets/javascripts/haveSquares/writeTextInCanvas.js
+++ app/assets/javascripts/haveSquares/writeTextInCanvas.js
@@ -1,31 +1,3 @@
-function wrapSentence(context, text, x, y, maxWidth, lineHeight) {
-    var words = text.split(' ');
-    var line = '';
-
-    for(var n = 0; n < words.length; n++) {
-      var testLine = line + words[n] + ' ';
-      var metrics = context.measureText(testLine);
-      var testWidth = metrics.width;
-      if (testWidth > maxWidth && n > 0) {
-        context.fillText(line, x, y);
-        line = words[n] + ' ';
-        y += lineHeight;
-      }
-      else {
-        line = testLine;
-      }
-    }
-    context.fillText(line, x, y);
-}
-
-function wrapText(context, text, x, y, maxWidth, lineHeight){
-    var lines = text.split('\n');
-    for (var i = 0; i < lines.length; i++) {
-        wrapSentence(context, lines[i], x, y + lineHeight*i, maxWidth, lineHeight);
-    }
-}
-
-
 function scaleCanvas(canvas, ctx){
 
     devicePixelRatio = window.devicePixelRatio || 1,
@@ -56,8 +28,37 @@ function scaleCanvas(canvas, ctx){
     }
 }
 
-function writeTextInCanvas(canvas, text, colorText, font, lineHeight, colorBackground){
-    var ctx = canvas.getContext("2d");
+function wrapSentence(context, text, x, y, maxWidth, lineHeight) {
+    var words = text.split(' ');
+    var line = '';
+
+    for(var n = 0; n < words.length; n++) {
+      var testLine = line + words[n] + ' ';
+      var metrics = context.measureText(testLine);
+      var testWidth = metrics.width;
+      if (testWidth > maxWidth && n > 0) {
+        context.fillText(line, x, y);
+        line = words[n] + ' ';
+        y += lineHeight;
+      }
+      else {
+        line = testLine;
+      }
+    }
+    context.fillText(line, x, y);
+}
+
+function wrapText(context, text, x, y, maxWidth, lineHeight){
+    var lines = text.split('\n');
+    for (var i = 0; i < lines.length; i++) {
+        wrapSentence(context, lines[i], x, y, maxWidth, lineHeight);
+        y += lineHeight;
+    }
+    return y - lineHeight;
+}
+
+
+function writeTextInCanvas(canvas, ctx, text, colorText, font, lineHeight, colorBackground){
 
     ctx.beginPath();
     ctx.fillStyle = colorBackground;
@@ -67,7 +68,23 @@ function writeTextInCanvas(canvas, text, colorText, font, lineHeight, colorBackg
     ctx.font = lineHeight + "px " + font;
     ctx.fillStyle = colorText;
     var padding = 20;
-    wrapText(ctx, text, padding, padding + lineHeight, canvas.width - 2*padding, lineHeight + 10);
+
+    var x = padding;
+    var y = padding;
+    var textWidth = canvas.width - 2 * padding;
+    var textHeight = wrapText(ctx, text, x, y + lineHeight, textWidth, lineHeight + 10);
+
+    return {x: x, y: y, width: textWidth, height: textHeight};
+};
+
+
+function surroundText(ctx, x, y, width, height){
+    ctx.beginPath();
+    ctx.setLineDash([5]);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth="2";
+    ctx.rect(x, y, width, height);
+    ctx.stroke();
 };
 
 
@@ -78,7 +95,14 @@ $(function(){
     var colorBackground = $('#iconBackground');
 
     var canvas = $('#square-where-we-draw');
+    var ctx = canvas.get(0).getContext("2d");
     var writeDiv = $('#writeTextDiv');
+
+    // dimension of the text
+    var x = null;
+    var y = null;
+    var width = null;
+    var height = null;
 
     // if we click in the canvas, appears the textArea where we can draw
     canvas.click(function(){
@@ -89,9 +113,25 @@ $(function(){
         $('#textColorSelector').click();
     });
 
+    canvas.mousedown(function(e){
+        if(x != null){
+            var coords = this.relMouseCoords(e);
+            //if the mouse is inside the canvas
+            if(x <= coords.x && coords.x <= x + width && y <= coords.y && coords.y <= y + height){
+                surroundText(ctx, x, y, width, height);
+            }
+        }
+    });
+
+
+    // When we click "ok", the text we have entered is written over the canvas
     $('#writeTextButton').click(function(){
         writeDiv.hide("slow");
-        writeTextInCanvas(canvas.get(0), text.val(), colorText.css('color'), "Calibri,Geneva,Arial", 30, colorBackground.css('color'));
+        var txt = writeTextInCanvas(canvas.get(0), ctx, text.val(), colorText.css('color'), "Calibri,Geneva,Arial", 30, colorBackground.css('color'));
+        x = txt.x;
+        y = txt.y;
+        width = txt.width;
+        height = txt.height;
         var dataURL = canvas.get(0).toDataURL();
     });
 });
--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -62,9 +62,7 @@ object Api extends Controller {
             addSelectedSquare(idCurrentMS, idUser, selectedSquare.idxSquare, selectedSquare.img) match {
               case 200 => {
                 //TODO: Envoyer les email
-                Redirect(routes.Application.home()).flashing(
-                  "login" -> "success"
-                ) // TODO: montrer qu'on a bien selectionné son petit carré.
+                Redirect(routes.Application.home()) // TODO: montrer qu'on a bien selectionné son petit carré.
               }
               case _ => Redirect(routes.Application.contact()).flashing(
                 "mainError" -> "Le formulaire pour pour selectionner les petits carree ne sont pas bon"
@@ -99,68 +97,4 @@ object Api extends Controller {
     }
   }
 
-
-
-//  def selectSquares = Action{ implicit request =>
-//    getUserId match {
-//      case None => {
-//        Redirect(routes.Application.login()).flashing(
-//          "haveToLogin" -> ""
-//        )
-//      }
-//      case Some(idUser) => {
-//        selectedSquaresForm.bindFromRequest.fold(
-//          errorsForm => {
-//            Redirect(routes.Application.contact()).flashing(
-//              "mainError" -> "Le formulaire pour pour selectionner les petits carree ne sont pas bon"
-//            )
-//          },
-//          selectedSquaresSting => {
-//            // TODO: partie dure avec la concurnce et tout le bordel + a optimiser
-//
-//            val MSOp = DB.query[Square].whereEqual("id", selectedSquaresSting.idMS).fetchOne()
-//            MSOp match {
-//              case null => Redirect(routes.Application.contact()).flashing(
-//                "mainError" -> "Le carré souhaité pour la selection des petits carré n'existe pas"
-//              )
-//              case Some(ms) => {
-//                val selectedSquares :  Seq[(Int, Int)] = stringToSelectedSquare(selectedSquaresSting.seq)
-//
-//                // verify that all the selected Squares are free
-//                val valid = selectedSquares.forall{ case (idx, color) =>
-//                  ms.squares(idx)._1 == -1
-//                }
-//                if(valid){
-//                  DB.save(ms.copy(squares = ms.squares.zipWithIndex.map{case (tupple, i) => {
-//                    val selectedSquare = selectedSquares.filter{
-//                      case (idx, _) => idx == i
-//                    }
-//                    if(selectedSquare.nonEmpty) {
-//                      (idUser, selectedSquare.head._2)
-//                    }
-//                    else tupple
-//                  }}))
-//                  // Redirect to the same square
-//                  Redirect(routes.Application.haveSquares( selectedSquaresSting.idMS))
-//                }
-//                else{
-//                  Redirect(routes.Application.contact()).flashing(
-//                    "mainError" -> "Les petits carré souhaité sont déjà pris"
-//                  )
-//                }
-//              }
-//            }
-//          }
-//        )
-//      }
-//    }
-//  }
-
-// must be format 1,2~3,3 avec le premier chiffre, l'indice et l;autre la couleur
-  def stringToSelectedSquare(s: String): Seq[(Int, Int)] = {
-    s.split("~").map{ a =>
-      val t = a.split(",")
-      (t(0).toInt, t(1).toInt)
-    }
-  }
 }
