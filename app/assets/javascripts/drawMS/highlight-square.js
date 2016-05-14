dc6592640be5bad03de00811f7b8bb0f93f9ff60
Lauriane Mollier <mollierlauriane@gmail.com>
2016-05-14 14:58:43 +0300

    Add legend without css

--- app/assets/javascripts/drawMS/highlight-square.js
+++ app/assets/javascripts/drawMS/highlight-square.js
@@ -2,7 +2,7 @@
 // x and y: the coordinates the the top-left corner
 // edgeLength: the length of the egde of the square
 // highlight the square
-function highlight(x, y, littleSL, ctx, color){
+function highlight(x, y, littleSL, ctx){
 
     var lineWidth = 3;
 
@@ -23,7 +23,7 @@ function highlightSquares(canvasToDraw, idxSquares, nbSquaresOneEdge, littleSL){
 
     for(var i=0; i<idxSquares.length; ++i){
         var coord = coordTopLeft(idxSquares[i], nbSquaresOneEdge, littleSL);
-        highlight(coord.x, coord.y, littleSL, cxt, "red");
+        highlight(coord.x, coord.y, littleSL, cxt);
 
     }
 
